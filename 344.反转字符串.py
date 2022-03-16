2790974c6f38d116f5d1ead364ab42fc2d48f30c
xing-ye <2026729306@qq.com>
2022-03-16 10:52:37 +0800

    比for循环时间复杂度低的方法

---
+++ 344.反转字符串.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=344 lang=python3
+#
+# [344] 反转字符串
+#
+
+# @lc code=start
+
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left,right=0,len(s)-1
+        while left<right:
+            s[left],s[right]=s[right],s[left]
+            left+=1
+            right-=1
+# @lc code=end
+'''
+这种方法相比于使用for循环更加通俗易懂，时间按复杂度也更低
+推荐使用
+'''
+
